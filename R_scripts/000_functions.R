15c8edf2b7fa658f9fff358dc28be7a5d0062174
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-05-07 07:29:46 -0500

    calc_change_cumu now returns a list and has an error message

--- R_scripts/000_functions.R
+++ R_scripts/000_functions.R
@@ -24,40 +24,39 @@ height_to_mm <- function(data){
 #### Cumulative change (change since first reading)
 ######################################################
 
-## calculate all levels of cumulative change, in one function?
-## returns three data frames (puts them into the Global Environment)
 
-# test
-if(exists('dat')){
-change_cumu_test_pin <<- dat %>%
-    group_by(reserve, set_id, arm_position, pin_number) %>%
-    mutate(cumu = pin_height - pin_height[min(which(!is.na(pin_height)))]) %>% ##### need to make this the first pin reading that's not NA - not just [1]
-    select(-pin_height) %>%
-    ungroup()
-
-change_cumu_test_arm <<- change_cumu_test_pin %>%
-    group_by(reserve, set_id, arm_position, date) %>%
-    select(-pin_number) %>%
-    summarize(mean_cumu = mean(cumu, na.rm = TRUE),
-              sd_cumu = sd(cumu, na.rm = TRUE),
-              se_cumu = sd(cumu, na.rm = TRUE)/sqrt(length(!is.na(cumu)))) %>%
-    ungroup()
-}
-
-## going to need better documentation, like what kind of data frame is needed as input (long); what column names are required; maybe some if statements to throw errors
-
+#' Calculate Cumulative Change at a SET
+#'
+#' @param dat a data frame with one row per pin reading, and the following columns, named exactly: date, set_id, arm_position, pin_number, pin_height
+#'
+#' @return a list of three tibbles: one each for pin, arm, and set level calculations. Pin level change is calculated first, as the difference between a pin reading and the reading from the earliest date at that set_id--arm--pin. If the first reading was NA, the entire pin's cumulative readings will be NA. The column name in the $pin tibble is "cumu". For every date of a pin reading, this calculated value will exist. On the first date, it is 0. Cumulative pin changes are then averaged to the arm position level on each date, excluding NAs. St Deviation and St Error are also calculated. There is one calculated value for every arm on every reading date. These columns in the $arm tibble are "mean_cumu", "sd_cumu", and "se_cumu". The cumulative arm changes are then averaged to the SET level, also with st dev and st err. There is one calculated value for every SET on every reading date. The columns in the $set tibble are again "mean_cumu", "sd_cumu", and "se_cumu".
+#' @export
+#'
+#' @examples
 calc_change_cumu <- function(dat) {
+    
+    ## conditions: have correct columns in data frame
+    ## stop and give an informative message if this isn't met
+    req_clms <- c("set_id", "arm_position", "pin_number", "pin_height", "date")
+    
+    if(sum(req_clms %in% names(dat)) != length(req_clms)){
+        stop(paste("Your data frame must have the following columns, with these names, but is missing at least one:", paste(req_clms, collapse = ", ")))
+    }
+    
+    
+    ## calculations
+    
     # by pin
-    change_cumu_pin <<- dat %>%
-        group_by(reserve, set_id, arm_position, pin_number) %>%
+    change_cumu_pin <- dat %>%
+        group_by(set_id, arm_position, pin_number) %>%
         mutate(cumu = pin_height - pin_height[1]) %>% ##### if there are nas in the first pin reading, maybe those pins should be excluded from further aggregation (at least this type of agg) - this will make those pins NA all the way through
         # mutate(cumu = pin_height - pin_height[min(which(!is.na(pin_height)))]) %>% ##### subtract off the first pin reading that's not NA
         select(-pin_height) %>%
         ungroup()
     
     # pins averaged up to arms
-    change_cumu_arm <<- change_cumu_pin %>%
-        group_by(reserve, set_id, arm_position, date) %>%
+    change_cumu_arm <- change_cumu_pin %>%
+        group_by(set_id, arm_position, date) %>%
         select(-pin_number) %>%
         summarize(mean_cumu = mean(cumu, na.rm = TRUE),
                   sd_cumu = sd(cumu, na.rm = TRUE),
@@ -65,14 +64,15 @@ calc_change_cumu <- function(dat) {
         ungroup()
     
     # arms averaged up to SETs
-    change_cumu_set <<- change_cumu_arm %>%
-        group_by(reserve, set_id, date) %>%
+    change_cumu_set <- change_cumu_arm %>%
+        group_by(set_id, date) %>%
         select(-arm_position, mean_value = mean_cumu) %>%
         summarize(mean_cumu = mean(mean_value, na.rm = TRUE),
                   sd_cumu = sd(mean_value, na.rm = TRUE),
                   se_cumu = sd(mean_value, na.rm = TRUE)/sqrt(length(!is.na(mean_value)))) %>%
         ungroup()
     
+    return(list(pin = change_cumu_pin, arm = change_cumu_arm, set = change_cumu_set))
 }
 
 
