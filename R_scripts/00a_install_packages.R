f4d27a71e411bac78fc6ab45d1f073677517c8b0
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-05-30 13:13:54 -0500

    added pkg installation and check scripts

---
+++ R_scripts/00a_install_packages.R
@@ -0,0 +1,23 @@
+# Install packages
+# THIS ONLY NEEDS TO BE RUN ONCE
+
+################ INSTRUCTIONS ##################################
+### Use Ctrl+A (Windows) / Cmd+A (Mac)   to select this entire script
+### Then Ctrl+Enter / Cmd+Enter  to run it
+
+### After this completes, run the script '00b_check_packages.R'
+### to make sure everything installed properly
+################################################################
+
+
+# identify needed packages
+pkg_wrangle <- c("dplyr", "here", "janitor", "lubridate", "purrr", "readr", "readxl", "stringr")
+pkg_interact <- c("DT", "ggplot2", "plotly", "shiny", "leaflet")
+pkg_analyze <- c("rmarkdown", "broom", "flextable")
+
+# install packages
+install.packages(c(pkg_wrangle, pkg_interact, pkg_analyze))
+
+# also need dev version of tidyr
+install.packages("devtools")
+devtools::install_github("tidyverse/tidyr")
---
+++ R_scripts/00b_check_packages.R
@@ -0,0 +1,47 @@
+################ INSTRUCTIONS ##################################
+### Use Ctrl+A (Windows) / Cmd+A (Mac)   to select this entire script
+### Then Ctrl+Enter / Cmd+Enter  to run it
+### Then check the output in your console and see if you 
+### need to re-install any packages
+
+### If you do, I recommend trying one at a time, using the command
+### install.packages("package_name_here")
+
+### Then re-run this script to make sure everything worked
+
+### If you have any problems, contact Kim
+
+################################################################
+
+
+# list of packages we need to have installed for our workflow
+pkg_wrangle <- c("dplyr", "here", "janitor", "lubridate", "purrr", "readr", "readxl", "stringr")
+pkg_interact <- c("DT", "ggplot2", "plotly", "shiny", "leaflet")
+pkg_analyze <- c("rmarkdown", "broom", "flextable")
+
+
+# glue the sub-categories together into one vector of packages we need
+pkgs_needed <- c(pkg_wrangle, pkg_interact, pkg_analyze, "tidyr")
+
+
+
+## now try loading each one and see if it works
+# set up an output vector
+pkg_result <- vector("logical", length(pkgs_needed))
+# loop through the needed packages
+for(i in seq_along(pkgs_needed)){
+    # try to load the package and report whether it works
+    # record that TRUE or FALSE in the pkg_result vector
+    pkg_result[i] <- library(pkgs_needed[i], character.only = TRUE, quietly = TRUE, logical.return = TRUE)
+}
+
+
+# make a vector of missing packages:
+# pkgs_needed that failed to load
+pkgs_missing <- pkgs_needed[!pkg_result]
+
+
+# print the results to the console
+if(length(pkgs_missing) == 0){
+    message("\n \nAll required packages are installed and loading properly! \n \n")
+} else{ message("\n \nYou need to install the following packages: "); cat(pkgs_missing, sep="\n") }
