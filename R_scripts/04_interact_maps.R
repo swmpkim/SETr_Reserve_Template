a0ccab2a59c33e6890b913588868a40b0a9039f3
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-06-04 12:19:59 -0500

    made choices interactive

--- R_scripts/04_interact_maps.R
+++ R_scripts/04_interact_maps.R
@@ -1,5 +1,12 @@
 ## map-making
 
+## TO-DOs:
+# put titles on the maps
+# make legends for the maps
+# figure out how to make this easy for an end-user
+# (maybe the guts of this script get sourced?)
+# see if information can pop up when an arrow is clicked on
+
 
 #### INSTRUCTIONS ####
 
@@ -12,26 +19,24 @@ library(dplyr)
 dat <- read.csv(here::here("data", "intermediate", "rate_summary.csv"))
 
 
-# map difference from 0
-# arrow for direction; color for whether CI excludes 0?
+# map differences
+# dir_0 is comparison to 0
+# dir_slr is comparison to SLR
+# arrow for direction; color for whether CIs overlap (gray if they do; red/blue if differences are "significant")
 # join coordinates with the rate results, and categorize the rates
-# if confidence intervals don't overlap 0, it's a significant increase or decrease
-# if CIs do overlap 0, it's nonsig increase or decrease based on sign of rate
 to_map <- dat %>%
-    mutate(dir_sig = case_when(CI_high < 0 ~ "dec_sig",
+    mutate(dir_0 = case_when(CI_high < 0 ~ "dec_sig",
                                CI_low > 0  ~ "inc_sig",
                                rate < 0 ~ "dec_nonsig",
                                rate > 0 ~ "inc_nonsig",
+                               TRUE ~ "nonsig"),
+           dir_slr = case_when(CI_high < slr_CI_low ~ "dec_sig",
+                               CI_low > slr_CI_high  ~ "inc_sig",
+                               rate < slr_rate ~ "dec_nonsig",
+                               rate > slr_rate ~ "inc_nonsig",
                                TRUE ~ "nonsig")) %>% 
     rename(lat = latitude_dec_deg,
            long = longitude_dec_deg)
-
-# set up the indices of which SET gets which icon, based on diff or not
-inc_sig_index <- which(to_map$dir_sig == "inc_sig")
-dec_sig_index <- which(to_map$dir_sig == "dec_sig")
-inc_nonsig_index <- which(to_map$dir_sig == "inc_nonsig")
-dec_nonsig_index <- which(to_map$dir_sig == "dec_nonsig")
-nonsig_index <- which(to_map$dir_sig == "nonsig")
 
 
 # read in images to use as map icons
@@ -41,67 +46,90 @@ icon_inc_nonsig_path <- here::here("img", "gray_up_arrow.png")
 icon_dec_nonsig_path <- here::here("img", "gray_down_arrow.png")
 icon_nonsig_path <- here::here("img", "gray_dash.png")
 
-
 # turn them into icons
 icon_inc_sig <- makeIcon(iconUrl = icon_inc_sig_path, 
-                      iconWidth = 30, iconHeight = 35)
+                         iconWidth = 30, iconHeight = 35)
 icon_dec_sig <- makeIcon(iconUrl = icon_dec_sig_path, 
-                      iconWidth = 30, iconHeight = 35)
+                         iconWidth = 30, iconHeight = 35)
 icon_inc_nonsig <- makeIcon(iconUrl = icon_inc_nonsig_path, 
-                         iconWidth = 30, iconHeight = 35)
+                            iconWidth = 30, iconHeight = 35)
 icon_dec_nonsig <- makeIcon(iconUrl = icon_dec_nonsig_path, 
-                         iconWidth = 30, iconHeight = 35)
+                            iconWidth = 30, iconHeight = 35)
 icon_nonsig <- makeIcon(iconUrl = icon_nonsig_path, 
                         iconWidth = 25, iconHeight = 12)
 
+# specify what these colors are, for the legends
+map_pal <- c("red", "blue", "gray")
 
-# make the map template
+
+
+# build the map
 m <- leaflet(to_map,
              options = leafletOptions(minZoom = 0, maxZoom = 25)) %>%
-        addScaleBar() %>%
-        addCircleMarkers(lng = ~long[nonsig_index],
-                     lat = ~lat[nonsig_index],
-                     radius = 8,
-                     color = "gray40") %>%
+    ### base layer options
+    addProviderTiles(leaflet::providers$Esri.WorldGrayCanvas, 
+                     group = "ESRI World Gray Canvas") %>% 
+    addProviderTiles(leaflet::providers$Esri.WorldTopoMap, 
+                     group = "ESRI World Topo Map") %>% 
+    addProviderTiles(leaflet::providers$Esri, 
+                     group = "ESRI default")%>% 
+    ### Compared to 0 
+    addMarkers(icon = icon_nonsig,
+               lng = ~long[to_map$dir_0 == "nonsig"],
+               lat = ~lat[to_map$dir_0 == "nonsig"],
+               group = "Compared to 0") %>%
     addMarkers(icon = icon_inc_sig,
-               lng = ~long[inc_sig_index],
-               lat = ~lat[inc_sig_index]) %>%
+               lng = ~long[to_map$dir_0 == "inc_sig"],
+               lat = ~lat[to_map$dir_0 == "inc_sig"],
+               group = "Compared to 0") %>%
     addMarkers(icon = icon_dec_sig,
-               lng = ~long[dec_sig_index],
-               lat = ~lat[dec_sig_index]) %>% 
+               lng = ~long[to_map$dir_0 == "dec_sig"],
+               lat = ~lat[to_map$dir_0 == "dec_sig"],
+               group = "Compared to 0") %>% 
     addMarkers(icon = icon_inc_nonsig,
-               lng = ~long[inc_nonsig_index],
-               lat = ~lat[inc_nonsig_index]) %>%  
+               lng = ~long[to_map$dir_0 == "inc_nonsig"],
+               lat = ~lat[to_map$dir_0 == "inc_nonsig"],
+               group = "Compared to 0") %>%  
     addMarkers(icon = icon_dec_nonsig,
-               lng = ~long[dec_nonsig_index],
-               lat = ~lat[dec_nonsig_index])
+               lng = ~long[to_map$dir_0 == "dec_nonsig"],
+               lat = ~lat[to_map$dir_0 == "dec_nonsig"],
+               group = "Compared to 0") %>% 
+    ### Compared to SLR 
+    addMarkers(icon = icon_nonsig,
+               lng = ~long[to_map$dir_slr == "nonsig"],
+               lat = ~lat[to_map$dir_slr == "nonsig"],
+               group = "Compared to SLR") %>%
+    addMarkers(icon = icon_inc_sig,
+               lng = ~long[to_map$dir_slr == "inc_sig"],
+               lat = ~lat[to_map$dir_slr == "inc_sig"],
+               group = "Compared to SLR") %>%
+    addMarkers(icon = icon_dec_sig,
+               lng = ~long[to_map$dir_slr == "dec_sig"],
+               lat = ~lat[to_map$dir_slr == "dec_sig"],
+               group = "Compared to SLR") %>% 
+    addMarkers(icon = icon_inc_nonsig,
+               lng = ~long[to_map$dir_slr == "inc_nonsig"],
+               lat = ~lat[to_map$dir_slr == "inc_nonsig"],
+               group = "Compared to SLR") %>%  
+    addMarkers(icon = icon_dec_nonsig,
+               lng = ~long[to_map$dir_slr == "dec_nonsig"],
+               lat = ~lat[to_map$dir_slr == "dec_nonsig"],
+               group = "Compared to SLR") %>%
+    ### control which layers can be shown
+    addLayersControl(
+        baseGroups = c("ESRI World Gray Canvas", "ESRI World Topo Map", "ESRI default"),
+        overlayGroups = c("Compared to 0", "Compared to SLR"),
+        options = layersControlOptions(collapsed = FALSE)
+    ) %>% 
+    hideGroup("Compared to SLR") %>% 
+    ### dress up the map
+    addScaleBar() %>%
+    addLegend(position = "bottomright",
+              colors = map_pal,
+              values = c(1:length(map_pal)),
+              labels = c("significantly lower", "significantly higher", "nonsignificant"))
+
+# print the map
+m
 
 
-## print the map with some different backgrounds:
-
-# Esri World Gray Canvas tiles
-m %>%
-    addProviderTiles(leaflet::providers$Esri.WorldGrayCanvas) 
-
-
-# Esri topo map (kim's favorite, but might not work everywhere)
-m %>%
-    addProviderTiles(leaflet::providers$Esri.WorldTopoMap)
-
-####################################################################
-
-# leaflet's default tiles
-m %>%
-    addTiles()
-
-
-# default Esri tiles
-m %>%
-    addProviderTiles(leaflet::providers$Esri)
-
-# NatGeo tiles
-m %>%
-    addProviderTiles(leaflet::providers$Esri.NatGeoWorldMap)
-
-m %>%
-    addProviderTiles(leaflet::providers$OpenTopoMap)
