b069525b37d40ae5fdcfbc031d0d761646fbd6f3
swmpkim <kimberly.cressman@dmr.ms.gov>
2020-02-25 09:26:50 -0600

    stats wording update and added map log file

--- R_scripts/sourced/004_map_making_static.R
+++ R_scripts/sourced/004_map_making_static.R
@@ -1,6 +1,10 @@
-# run this at the end of the Word report script (after 005)
+# run this at the end of the Word report script (after 002)
 library(leaflet)
 library(mapview)
+
+# save a log file just in case
+log_name <- paste0(Sys.Date(), "_004mapsstatic_logfile.txt")
+writeLines(capture.output(sessionInfo()), here::here("R_output", "log_files", log_name))
 
 # map differences
 # dir_0 is comparison to 0
